306854478b3bf6653a5eb343a29cede083a9b6d2
Nelson Dane <danenb@clarkson.edu>
2023-03-08 00:58:31 -0500

    uart + multithreading?

--- python/driver.py
+++ python/driver.py
@@ -1,8 +1,24 @@
-import serial
 from uart import *
+import threading
+from time import sleep
 
 uart = create_uart()
 
+def read(uart):
+    while True:
+        return read_uart(uart)
+
+data = "Hello World"
+t1 = threading.Thread(target=read, args=(uart,))
+t1.start()
+
+t1.join()
+
 while True:
-    data = read_uart(uart)
-    print(data)
+    try:
+        print(read(uart))
+        write_uart(uart, data)
+    except KeyboardInterrupt:
+        break
+
+uart.close()
--- python/uart.py
+++ python/uart.py
@@ -3,25 +3,34 @@ import serial
 
 # Constants for UART
 BAUD_RATE = 9600
-PORT = '/dev/ttyUSB0'
+PORT = 'COM3'
 
 # Create UART object
 def create_uart():
-    # input("Connect?")
-    # return True
-    return serial.Serial(PORT, BAUD_RATE)
+    try:
+        uart = serial.Serial(PORT, BAUD_RATE, write_timeout=1, timeout=1)
+        print("Connected to UART.")
+        return uart
+    # Except permission denied
+    except PermissionError:
+        print("Permission denied. Please close any other programs using the UART.")
+        return None
 
 # Write to UART
 def write_uart(uart, data):
-    print(data)
-    #uart.write(bytes(data, 'utf-8'))
+    print(f"Write: {data}")
+    try:
+        uart.write(bytes(data, 'utf-8'))
+    except serial.SerialTimeoutException:
+        print("Write timeout.")
 
 # Read from UART
 def read_uart(uart):
-    # test = input("Read from UART: ")
-    # return test
-    return uart.readline().decode('utf-8')
+    data = uart.readline().decode('utf-8')
+    print(f"Read: {data}")
+    return data
 
 # Close UART
 def close_uart(uart):
+    print("Closing UART.")
     uart.close()
